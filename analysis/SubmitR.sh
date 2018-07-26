87ce0717a384ef4adf7f27f99ce586b939de7696
bw4sz <benweinstein2010@gmail.com>
2018-07-26 12:42:34 -0700

    ranks and tasks are confusing on slurm

--- analysis/SubmitR.sh
+++ analysis/SubmitR.sh
@@ -4,8 +4,8 @@
 #SBATCH --mail-user=ben.weinstein@weecology.org   # Where to send mail
 #SBATCH --account=ewhite
 #SBATCH --qos=ewhite-b
-#SBATCH --ntasks=1                 # Number of MPI ranks
-#SBATCH --cpus-per-task=20            # Number of cores per MPI rank
+#SBATCH --ntasks=2                 # Number of MPI ranks
+#SBATCH --cpus-per-task=10            # Number of cores per MPI rank
 #SBATCH --mem-per-cpu=20GB
 #SBATCH --time=12:00:00       #Time limit hrs:min:sec
 #SBATCH --output=/home/b.weinstein/logs/detection.out   # Standard output and error log
