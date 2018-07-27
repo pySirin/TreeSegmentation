64a2a89cd84c1e1f2af37e4d8c765eb34f81033b
bw4sz <benweinstein2010@gmail.com>
2018-07-27 15:02:35 -0700

    make convert names more resiliant among sites

--- R/convert_names.R
+++ R/convert_names.R
@@ -7,19 +7,34 @@
 #' @param lidar Character. Lidar pathname
 #' @param hyperspectral hyerspectral pathname
 #' @export
-convert_names<-function(from,to,lidar=NULL,rgb=NULL,hyperspectral=NULL){
+convert_names<-function(from,to,lidar=NULL,rgb=NULL,hyperspectral=NULL,site='OSBS'){
 
   if(from=="rgb" & to=="lidar"){
     #Get corresponding lidar tile
     geo_index<-stringr::str_match(rgb,"_(\\d+_\\d+)_image")[,2]
+    indices<-strsplit(rgb,"_")
+    site<-indices[[1]][[5]]
+    domain<-indices[[1]][[4]]
 
-    fn<-paste("NEON_D03_OSBS_DP1_",geo_index,"_classified_point_cloud.laz",sep="")
+    fn<-paste("NEON_",domain,"_",site,"_DP1_",geo_index,"_classified_point_cloud.laz",sep="")
     return(fn)
   }
 
   if(from=="lidar" & to == "rgb"){
     geo_index<-stringr::str_match(lidar,"_(\\d+_\\d+)_classified")[,2]
-    fn<-paste("2017_OSBS_3_",geo_index,"_image.tif",sep="")
+    indices<-strsplit(lidar,"_")
+    site<-indices[[1]][[5]]
+    domain<-indices[[1]][[4]]
+
+    if(site=="OSBS"){
+      fn<-paste("2017",site,"_3_",geo_index,"_image.tif",sep="")
+    }
+
+    if(site=="SJER"){
+      fn<-paste("NEON_",domain,"_",site,"_DP3_",geo_index,"_image.tif",sep="")
+    }
+
+
     return(fn)
   }
 
