f47c15c3fe5a9c87f0654988e64a44e77f934f11
bw4sz <benweinstein2010@gmail.com>
2018-07-27 11:34:00 -0700

    detection training SJER

--- analysis/SubmitR.sh
+++ analysis/SubmitR.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=ben.weinstein@weecology.org   # Where to send mail
 #SBATCH --account=ewhite
 #SBATCH --qos=ewhite-b
-#SBATCH --ntasks=2                 # Number of MPI ranks
+#SBATCH --ntasks=1                 # Number of MPI ranks
 #SBATCH --cpus-per-task=10            # Number of cores per MPI rank
 #SBATCH --mem-per-cpu=20GB
 #SBATCH --time=12:00:00       #Time limit hrs:min:sec
@@ -13,4 +13,4 @@
 
 #This is a generic R submission script
 module load gcc/6.3.0 R/3.4.3 gdal/2.2.1
-Rscript training.R
+Rscript detection_training.R
--- analysis/detection_training.R
+++ analysis/detection_training.R
@@ -7,16 +7,18 @@ library(dplyr)
 testing=F
 
 if(testing){
-  #path<-"../data/2017/Lidar/OSBS_006.laz"
+  path<-"../data/2017/Lidar/OSBS_006.laz"
   #path<-"../data/training/NEON_D03_OSBS_DP1_407000_3291000_classified_point_cloud.laz"
   detection_training(path)
  } else{
 
-  #lidar data dir
-  lidar_dir<-"/orange/ewhite/NeonData/2017_Campaign/D03/OSBS/L1/DiscreteLidar/Classified_point_cloud/"
-  rgb_dir<-"/orange/ewhite/b.weinstein/NEON/D03/OSBS/DP1.30010.001/2017/FullSite/D03/2017_OSBS_3/L3/Camera/Mosaic/V01/"
-  itcs_path<-"/orange/ewhite/b.weinstein/ITC"
-  lidar_files<-list.files(lidar_dir,full.names = T,pattern=".laz")
+  #SJER lidar data dir
+  lidar_dir<-"/orange/ewhite/NeonData/SJER/DP1.30003.001/2017/FullSite/D17/2017_SJER_2/L1/DiscreteLidar/ClassifiedPointCloud/"
+
+  #lidar_dir<-"/orange/ewhite/NeonData/2017_Campaign/D03/OSBS/L1/DiscreteLidar/Classified_point_cloud/"
+  #rgb_dir<-"/orange/ewhite/b.weinstein/NEON/D03/OSBS/DP1.30010.001/2017/FullSite/D03/2017_OSBS_3/L3/Camera/Mosaic/V01/"
+  #itcs_path<-"/orange/ewhite/b.weinstein/ITC"
+  lidar_files<-list.files(lidar_dir,full.names = T,pattern="cloud.laz")
 
   cl<-makeCluster(10)
   registerDoSNOW(cl)
@@ -24,7 +26,8 @@ if(testing){
   results<-foreach::foreach(x=1:length(lidar_files),.packages=c("TreeSegmentation")) %dopar%{
 
     #check if tile can be processed
-    flag<-check_tile(itcs_path=itcs_path,lidar_path = lidar_files[x],rgb_dir=rgb_dir)
+    #flag<-check_tile(itcs_path=itcs_path,lidar_path = lidar_files[x],rgb_dir=rgb_dir)
+    flag=TRUE
 
     if(flag){
       detection_training(path=lidar_files[x])
