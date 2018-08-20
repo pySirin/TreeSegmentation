f6ca9df0e4f6f933a1c29e5e58a39b9694bdcbbe
bw4sz <benweinstein2010@gmail.com>
2018-08-20 11:32:36 -0700

    lidar crops

---
+++ analysis/Crop_RGB_NEONPLOTS.R
@@ -0,0 +1,90 @@
+### Clip RGB Data Based on Neon Plots ###
+library(maptools)
+library(raster)
+library(TreeSegmentation)
+library(doSNOW)
+library(foreach)
+library(parallel)
+library(rgdal)
+library(sf)
+
+plots<-st_read("data/NEONFieldSites/All_NEON_TOS_Plots_V4/All_Neon_TOS_Polygon_V4.shp")
+dat<-read.csv("data/Terrestrial/field_data.csv")
+OSBS<-dat %>% filter(siteID=="OSBS") %>% droplevels()
+OSBS_plots<-plots[plots$plotID %in% OSBS$plotID,]
+
+#Count trees, only keep basePlots
+Trees<-OSBS %>% group_by(plotID) %>% summarize(Trees=n())
+OSBS_trees<-OSBS_plots %>% inner_join(Trees) %>% filter(subtype=="basePlot")
+
+#Crop lidar by plot extent and write to file
+cl<-makeCluster(10)
+registerDoSNOW(cl)
+
+foreach(x=1:nrow(OSBS_trees),.packages=c("TreeSegmentation","sp","raster"),.errorhandling = "pass") %dopar% {
+  #plot(itcs[[x]])
+
+  #Look for corresponding tile
+  #get lists of rasters
+  inpath<-"/orange/ewhite/NeonData/OSBS/DP1.30010.001/2017/FullSite/D03/2017_OSBS_3/L3/Camera/Mosaic/V01"
+  fils<-list.files(inpath,full.names = T,pattern=".tif")
+  filname<-list.files(inpath,pattern=".tif")
+
+  #loop through rasters and look for intersections
+  for (i in 1:length(fils)){
+
+    #set counter for multiple tiles
+    j=1
+    #empty vector to hold tiles
+    matched_tiles <- vector("list", 10)
+
+    #load raster and check for overlap
+    r<-stack(fils[[i]])
+    do_they_intersect<-raster::intersect(extent(r),extent(itcs[[x]]))
+
+    #Do they intersect?
+    if(is.null(do_they_intersect)){
+      next
+    } else{
+      matched_tiles[[j]]<-r
+      j<-j+1
+
+      #do they intersect completely? If so, go to next tile
+      if(extent(do_they_intersect)==extent(itcs[[x]])){
+        break
+      }
+    }
+  }
+
+
+  #bind together tiles if matching more than one tile
+  matched_tiles<-matched_tiles[!sapply(matched_tiles,is.null)]
+
+  #If no tile matches, exit.
+  if(length(matched_tiles)==0){
+    return(paste("No matches ",unique(itcs[[x]]$Plot_ID)))
+  }
+
+  if(length(matched_tiles)>1){
+    tile_to_crop<-do.call(mosiac,matched_tiles)
+  } else{
+    tile_to_crop<-matched_tiles[[1]]
+  }
+
+  #Clip matched tile
+  #Create a window of equal size, centered
+  #center point
+
+  #create extent polygon
+  e<-extent(OSBS_trees[x,])
+
+  clipped_rgb<-raster::crop(tile_to_crop,e)
+
+  #filename
+  cname<-paste("/orange/ewhite/b.weinstein/NEON/OSBS/NEONPlots/Camera/L3/",unique(itcs[[x]]$Plot_ID),".tif",sep="")
+  print(cname)
+
+  #rescale to
+  writeRaster(clipped_rgb,cname,overwrite=T,datatype='INT1U')
+  return(cname)
+}
