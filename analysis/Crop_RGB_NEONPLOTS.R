d98480107a82d8b8c8ab72278cd1ba6c17a7720f
bw4sz <benweinstein2010@gmail.com>
2018-08-21 14:36:08 -0700

    try l1

--- analysis/Crop_RGB_NEONPLOTS.R
+++ analysis/Crop_RGB_NEONPLOTS.R
@@ -81,7 +81,7 @@ foreach(x=1:nrow(OSBS_trees),.packages=c("TreeSegmentation","sp","raster"),.erro
   clipped_rgb<-raster::crop(tile_to_crop,e)
 
   #filename
-  cname<-paste("/orange/ewhite/b.weinstein/NEON/OSBS/NEONPlots/Camera/L3/",plotid,".tif",sep="")
+  cname<-paste("/orange/ewhite/b.weinstein/NEON/OSBS/NEONPlots/Camera/L1/",plotid,".tif",sep="")
   print(cname)
 
   #rescale to
