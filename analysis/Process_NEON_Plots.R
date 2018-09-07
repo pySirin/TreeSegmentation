a3ba150c70a1e07794dd993c2a6dce687e74e8b8
bw4sz <benweinstein2010@gmail.com>
2018-09-07 09:54:06 -0700

    all sites

--- analysis/Process_NEON_Plots.R
+++ analysis/Process_NEON_Plots.R
@@ -7,12 +7,13 @@ library(dplyr)
 
 ###Download RGB and LIDAR tiles
 
-c("MOAB","BONA","CLBJ","HARV","KONZ","NIWO","OSBS","SJER","WREF","JORN","MLBS","ORNL","SCBI","STEI","TALL","WOOD")
-site="NIWO"
-fold<-paste("/orange/ewhite/NeonData/",site,sep="")
-byPointsAOP(dpID="DP1.30010.001",site=site,year="2017",check.size=F, savepath=fold,allSites=F)
-byPointsAOP(dpID="DP1.30003.001",site=site,year="2017",check.size=F, savepath=fold,allSites=F)
+site<-c("MOAB","BONA","CLBJ","HARV","KONZ","NIWO","OSBS","SJER","WREF","JORN","MLBS","ORNL","SCBI","STEI","TALL","WOOD")
+for(x in 1:length(site)){
+  fold<-paste("/orange/ewhite/NeonData/",site[x],sep="")
+  byPointsAOP(dpID="DP1.30010.001",site=site[x],year="2017",check.size=F, savepath=fold,allSites=F)
+  byPointsAOP(dpID="DP1.30003.001",site=site[x],year="2017",check.size=F, savepath=fold,allSites=F)
+  ##Cut Tiles
+  crop_rgb_plots(site[x])
+  crop_lidar_plots(site[x])
+}
 
-##Cut Tiles
-crop_rgb_plots(site)
-crop_lidar_plots(site)
