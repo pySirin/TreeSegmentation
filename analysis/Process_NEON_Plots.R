36a2414c3e3c5be8b111dd9cc122c1c324fa4b8c
bw4sz <benweinstein2010@gmail.com>
2018-09-08 13:29:33 -0700

    trying hyperspec

--- analysis/Process_NEON_Plots.R
+++ analysis/Process_NEON_Plots.R
@@ -5,15 +5,18 @@ library(neonUtilities)
 library(TreeSegmentation)
 library(dplyr)
 
-###Download RGB and LIDAR tiles
-#c("MOAB","BONA","CLBJ","HARV","KONZ","NIWO","OSBS","SJER","WOOD","UNDE","WREF","JORN","MLBS","ORNL","SCBI","STEI","TALL",
-site<-c("TOOL","SRER","PUUM","ONAQ","ORNL","GUAN","CPER","BART","ABBY","DCFS","DSNY","HEAL","JERC","LAJA","LENO","NOGP","RMNP","SOAP","TEAK","TREE","UKFS","UNDE")
+###Download RGB and LIDAR, HyperSpec tiles
+#site<-c()
+site<-c("TOOL","SRER","PUUM","ONAQ","ORNL","GUAN","CPER","BART","ABBY","DCFS","DSNY","HEAL","JERC","LAJA","LENO","NOGP","RMNP","SOAP","TEAK","TREE","UKFS","UNDE","MOAB","BONA","CLBJ","HARV","KONZ","NIWO","OSBS","SJER","WOOD","UNDE","WREF","JORN","MLBS","ORNL","SCBI","STEI","TALL")
+
 for(x in 1:length(site)){
   fold<-paste("/orange/ewhite/NeonData/",site[x],sep="")
-  byPointsAOP(dpID="DP1.30010.001",site=site[x],year="2017",check.size=F, savepath=fold,allSites=F)
-  byPointsAOP(dpID="DP1.30003.001",site=site[x],year="2017",check.size=F, savepath=fold,allSites=F)
+  #byPointsAOP(dpID="DP1.30010.001",site=site[x],year="2017",check.size=F, savepath=fold,allSites=F)
+  #byPointsAOP(dpID="DP1.30003.001",site=site[x],year="2017",check.size=F, savepath=fold,allSites=F)
+  byPointsAOP(dpID="DP1.30006.001",site=site[x],year="2017",check.size=F, savepath=fold,allSites=F)
+
   ##Cut Tiles
-  crop_rgb_plots(site[x])
-  crop_lidar_plots(site[x])
+  #crop_rgb_plots(site[x])
+  #crop_lidar_plots(site[x])
 }
 
