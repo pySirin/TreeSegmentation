04207cfae1e7353c13310df22cefd22546c6ec8c
bw4sz <benweinstein2010@gmail.com>
2018-09-21 09:07:11 -0700

    changing cpus

--- analysis/Process_NEON_Plots.R
+++ analysis/Process_NEON_Plots.R
@@ -9,14 +9,14 @@ library(doSNOW)
 sites<-c("ARIK","BARR","BART","BONA","CLBJ","CPER","CUPE","DEJU","DELA","DSNY","GRSM","GUAN",
 "GUIL","HARV","HEAL","HOPB","HOPB","JERC","JORN","KONZ","LAJA","LENO","LIRO","MCDI","MLBS","MOAB","NIWO","NOGP","OAES","OSBS","PRIN","REDB","RMNP","SCBI","SERC","SJER","SOAP","SRER","STEI","STER","TALL","TEAK","TOOL","UKFS","UNDE","WLOU","WOOD","WREF")
 
-cl<-makeCluster(10,outfile="")
+cl<-makeCluster(5,outfile="")
 registerDoSNOW(cl)
 
 foreach(x=1:length(site),.packages=c("neonUtilities","TreeSegmentation","dplyr"),.errorhandling = "pass") %dopar% {
   fold<-paste("/orange/ewhite/NeonData/",sites[x],sep="")
   #byPointsAOP(dpID="DP1.30010.001",site=sites[x],year="2017",check.size=F, savepath=fold,allSites=F)
   #byPointsAOP(dpID="DP1.30010.001",site=sites[x],year="2018",check.size=F, savepath=fold,allSites=F)
-  #byPointsAOP(dpID="DP1.30003.001",site=sites[x],year="2017",check.size=F, savepath=fold,allSites=F)
+  byPointsAOP(dpID="DP1.30003.001",site=sites[x],year="2018",check.size=F, savepath=fold,allSites=F)
   #byPointsAOP(dpID="DP1.30006.001",site=sites[x],year="2017",check.size=F, savepath=fold,allSites=F)
   ##Cut Tiles
   #crop_rgb_plots(sites[x])
