b0c3df5e33fd72f65bfdf7e40698457f51b49b37
bw4sz <benweinstein2010@gmail.com>
2018-09-30 14:09:45 -0700

    Wrong spot

--- R/ParallelDownload.R
+++
@@ -1,8 +0,0 @@
-### Download AOP data tiles in parallel
-
-#devtools::install_github("Weecology/Neon-Utilities/neonUtilities",dependencies=F)
-
-library(neonUtilities)
-
-ParallelFileAOP(dpID = "DP3.30010.001",site = "SJER",year="2018",check.size=F, savepath=fold,cores=5)
-ParallelFileAOP(dpID = "DP1.30003.001",site = "SJER",year="2018",check.size=F, savepath=fold,cores=5)
