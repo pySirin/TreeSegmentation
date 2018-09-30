4e4af6cd332f00ce3ec3233ad2ada54d6f854953
bw4sz <benweinstein2010@gmail.com>
2018-09-30 14:38:33 -0700

    not sure whats wrong to be honest

--- analysis/ParallelDownload.R
+++ analysis/ParallelDownload.R
@@ -3,6 +3,6 @@
 #devtools::install_github("Weecology/Neon-Utilities/neonUtilities",dependencies=F)
 
 library(neonUtilities)
-
+fold<-paste("/orange/ewhite/NeonData/",site="SJER",sep="")
 ParallelFileAOP(dpID = "DP3.30010.001",site = "SJER",year="2018",check.size=F, savepath=fold,cores=5)
 ParallelFileAOP(dpID = "DP1.30003.001",site = "SJER",year="2018",check.size=F, savepath=fold,cores=5)
