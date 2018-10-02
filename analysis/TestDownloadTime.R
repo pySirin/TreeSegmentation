ccfeca7670c1bf462746918e53e1b50982ad3d42
bw4sz <benweinstein2010@gmail.com>
2018-10-02 15:07:08 -0700

    print

--- analysis/TestDownloadTime.R
+++ analysis/TestDownloadTime.R
@@ -1,5 +1,4 @@
-#Test Download time
-
+#Test Download time for the NEON AOP API
 library(gdata)
 
 dpID = "DP1.30003.001"
@@ -15,42 +14,6 @@ month.urls <- unlist(avail$data$siteCodes$availableDataUrls)
 month.urls <- month.urls[grep(paste(site, year, sep="/"), month.urls)]
 
 #Define functions
-
-download_file<-function(file.urls.current,filepath,month.urls){
-  # copy zip files into folder
-  j <- 1
-  messages <- list()
-  while(j <= nrow(file.urls.current)) {
-    path1 <- strsplit(file.urls.current$URL[j], "\\?")[[1]][1]
-    pathparts <- strsplit(path1, "\\/")
-    path2 <- paste(pathparts[[1]][4:(length(pathparts[[1]])-1)], collapse="/")
-    newpath <- paste0(filepath, "/", path2)
-    
-    if(dir.exists(newpath) == F) dir.create(newpath, recursive = T)
-    dtime<-system.time(t <- try(downloader::download(file.urls.current$URL[j],
-                                  paste(newpath, file.urls.current$name[j], sep="/"),
-                                  mode="wb"), silent = T))
-    
-    downld.size <- sum(as.numeric(as.character(file.urls.current$size)), na.rm=T)
-    bytes_per_second<-downld.size / dtime[3]
-    speed<- humanReadable(bytes_per_second, units = "auto", standard = "SI")
-    
-    paste("Estimated download speed of",speed,"per second")
-    
-    if(class(t) == "try-error"){
-      writeLines("File could not be downloaded. URLs may have expired. Getting new URLs.")
-      file.urls.new <- getFileUrls(month.urls)
-      file.urls.current <- file.urls.new
-      writeLines("Continuing downloads.")}
-    if(class(t) != "try-error"){
-      messages[j] <- paste(file.urls.current$name[j], "downloaded to", newpath, sep=" ")
-      j = j + 1
-    }
-  }
-  writeLines(paste("Successfully downloaded ", length(messages), " files."))
-  writeLines(paste0(messages, collapse = "\n"))
-}
-
 getFileUrls <- function(m.urls){
   url.messages <- character()
   file.urls <- c(NA, NA, NA)
@@ -80,6 +43,41 @@ getFileUrls <- function(m.urls){
     file.urls <- file.urls[-1, ]
     return(file.urls)
   }
+}
+
+download_file<-function(file.urls.current,filepath,month.urls){
+  # copy zip files into folder
+  j <- 1
+  messages <- list()
+  while(j <= nrow(file.urls.current)) {
+    path1 <- strsplit(file.urls.current$URL[j], "\\?")[[1]][1]
+    pathparts <- strsplit(path1, "\\/")
+    path2 <- paste(pathparts[[1]][4:(length(pathparts[[1]])-1)], collapse="/")
+    newpath <- paste0(filepath, "/", path2)
+    
+    if(dir.exists(newpath) == F) dir.create(newpath, recursive = T)
+    dtime<-system.time(t <- try(downloader::download(file.urls.current$URL[j],
+                                  paste(newpath, file.urls.current$name[j], sep="/"),
+                                  mode="wb"), silent = T))
+    
+    downld.size <- sum(as.numeric(as.character(file.urls.current$size)), na.rm=T)
+    bytes_per_second<-downld.size / dtime[3]
+    speed<- humanReadable(bytes_per_second, units = "auto", standard = "SI")
+    
+    print(paste("Estimated download speed of",speed,"per second"))
+    
+    if(class(t) == "try-error"){
+      writeLines("File could not be downloaded. URLs may have expired. Getting new URLs.")
+      file.urls.new <- getFileUrls(month.urls)
+      file.urls.current <- file.urls.new
+      writeLines("Continuing downloads.")}
+    if(class(t) != "try-error"){
+      messages[j] <- paste(file.urls.current$name[j], "downloaded to", newpath, sep=" ")
+      j = j + 1
+    }
+  }
+  writeLines(paste("Successfully downloaded ", length(messages), " files."))
+  writeLines(paste0(messages, collapse = "\n"))
 }
 
 file.urls.current <- getFileUrls(month.urls)
